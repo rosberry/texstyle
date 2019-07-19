70af0f8dbb8052a38329ddf2dea96421fe815a91
Artem Novichkov <artem.novichkov@rosberry.com>
2019-07-19 09:24:57 +0600

    Add missing docs

--- Texstyle/Texstyle/Array+Text.swift
+++ Texstyle/Texstyle/Array+Text.swift
@@ -2,6 +2,10 @@
 //  Copyright © 2019 Rosberry. All rights reserved.
 //
 
+/// Strategy of separator styling for joining texts in array.
+///
+/// - left: Uses styles of left text.
+/// - right: Uses styles of right text.
 public enum SeparatorStrategy {
     case left
     case right
@@ -9,6 +13,12 @@ public enum SeparatorStrategy {
 
 public extension Array where Element: Text {
 
+    /// Returns a new text by concatenating the elements of the sequence,
+    /// adding the given separator between each element.
+    ///
+    /// - Parameter separatorText: A text to insert between each of the elements
+    ///   in this sequence. Default value is nil.
+    /// - Returns: A single, concatenated text.
     func joined(separatorText: Text? = nil) -> Text {
         if isEmpty {
             return Text(value: "", style: .init())
@@ -30,16 +40,25 @@ public extension Array where Element: Text {
         return text
     }
 
+    /// Returns a new text by concatenating the elements of the sequence,
+    /// adding the given separator between each element.
+    ///
+    /// - Parameters:
+    ///   - separator: A string to insert between each of the elements
+    ///   in this sequence.
+    ///   - strategy: The strategy of separator styling. Default value is left.
+    /// - Returns: A single, concatenated text.
     func joined(separator: String, strategy: SeparatorStrategy = .left) -> Text {
         if isEmpty {
             return Text(value: "", style: .init())
         }
+
         if count == 1 {
             return self[0]
         }
+
         var text: Text = self[0]
         for index in 1..<count {
-
             let separatorText: Text
             switch strategy {
             case .left:
