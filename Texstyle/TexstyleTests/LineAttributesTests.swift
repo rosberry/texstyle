c2ce644fa559cf8d7b25603f7b422d0b96d2bac7
Artem Novichkov <artem.novichkov@rosberry.com>
2019-11-26 14:50:00 +0600

    Add copy for line attributes

---
+++ Texstyle/TexstyleTests/LineAttributesTests.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2019 Rosberry. All rights reserved.
+//
+
+import XCTest
+@testable import Texstyle
+
+final class LineAttributesTests: XCTestCase {
+
+    func testCopy() {
+        //Given
+        let attributes = LineAttributes.random
+        //When
+        let copy = attributes.copy()
+        //Then
+        XCTAssertEqual(copy, attributes, "Copy is not equal to line attributes")
+    }
+}
