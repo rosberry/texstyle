d813333796845f1308d0cba6a5ec0b00f23c7242
Stas Klyukhin <stanislavak314@gmail.com>
2022-09-09 11:37:32 +0700

    update package swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Texstyle"
+        name: "Texstyle",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "Texstyle",
+            targets: ["Texstyle"]),
+    ],
+    targets: [
+        .target(name: "Texstyle")
+    ]
 )
